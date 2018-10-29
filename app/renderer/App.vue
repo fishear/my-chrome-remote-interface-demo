5d4c1970a1923c9b8441ee633b518831923ffb1a
yusha <yusha.icey@bytedance.com>
2018-10-29 21:11:48 +0800

    first commit

---
+++ app/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-chrome-remote-interface-demo'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
